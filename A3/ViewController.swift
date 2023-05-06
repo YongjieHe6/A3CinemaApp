01e9b3a791789a5d144c8e0dd91c43c22388d9b9
YongjieHe1 <100255887+YongjieHe1@users.noreply.github.com>
2023-05-06 21:31:50 +1000

    Initial Commit

---
+++ A3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A3
+//
+//  Created by 何永洁 on 6/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A3UITests/A3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  A3UITestsLaunchTests.swift
+//  A3UITests
+//
+//  Created by 何永洁 on 6/5/2023.
+//
+
+import XCTest
+
+final class A3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
